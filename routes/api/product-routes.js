9c26b1489a5b470f155af2b89387b8cc2527b05d
Steph Melanofridis <stephmelanofridis@bigpond.com>
2021-08-31 15:21:37 +1000

    Updated routes files

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -4,18 +4,38 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // The `/api/products` endpoint
 
 // get all products
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all products
-  // be sure to include its associated Category and Tag data
+  // be sure to include its associated Category and Tag data - Complete
+  try {
+    const productData = await Product.findAll({
+      include: [{ model: Category }, { model: Tag, through: ProductTag, as: 'linked_tags' }]
+    });
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(400).json(err);
+  };
 });
 
 // get one product
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find a single product by its `id`
-  // be sure to include its associated Category and Tag data
+  // be sure to include its associated Category and Tag data - Complete
+  try {
+    const productData = await Product.findByPk(req.params.id, {
+      include: [{ model: Category }, { model: Tag, through: ProductTag, as: 'linked_tags' }]
+    });
+    if (!productData) {
+      res.status(404).json({ message: 'No match with this ID. Please try again.' });
+      return;
+    }
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(400).json(err);
+  };
 });
 
-// create new product
+// create new product - Already filled in
 router.post('/', (req, res) => {
   /* req.body should look like this...
     {
@@ -90,7 +110,21 @@ router.put('/:id', (req, res) => {
 });
 
 router.delete('/:id', (req, res) => {
-  // delete one product by its `id` value
+  // delete one product by its `id` value - Complete
+  try {
+    const productData = await Product.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+    if (!productData) {
+      res.status(404).json({ message: 'No match with this ID. Please try again.' });
+      return;
+    }
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(400).json(err);
+  };
 });
 
 module.exports = router;
--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -3,26 +3,76 @@ const { Tag, Product, ProductTag } = require('../../models');
 
 // The `/api/tags` endpoint
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all tags
-  // be sure to include its associated Product data
+  // be sure to include its associated Product data - Complete
+  try {
+    const tagData = await Tag.findAll({
+      include: [{ model: Product, through: ProductTag, as: 'linked_tag' }]
+    });
+    res.status(200).json(tagData);
+  } catch (err) {
+    res.status(400).json(err);
+  };
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find a single tag by its `id`
-  // be sure to include its associated Product data
+  // be sure to include its associated Product data - Complete
+  try {
+    const tagData = await Tag.findByPk({
+      include: [{ model: Product, through: ProductTag, as: 'linked_tag' }]
+    });
+    if (!tagData) {
+      res.status(404).json({ message: 'No match with this ID. Please try again.' });
+      return;
+    }
+    res.status(200).json(tagData);
+  } catch (err) {
+    res.status(400).json(err);
+  };
 });
 
-router.post('/', (req, res) => {
-  // create a new tag
+router.post('/', async (req, res) => {
+  // create a new tag - Complete
+  const tagData = await Tag.create(req.body);
+  res.status(200).json(tagData);
 });
 
-router.put('/:id', (req, res) => {
-  // update a tag's name by its `id` value
+router.put('/:id', async (req, res) => {
+  // update a tag's name by its `id` value - Complete
+  try {
+    const tagData = await Tag.update({
+      where: {
+        id: req.params.id,
+      },
+    });
+    if (!tagData) {
+      res.status(404).json({ message: 'No match with this ID. Please try again.' });
+      return;
+    }
+    res.status(200).json(tagData);
+  } catch (err) {
+    res.status(400).json(err);
+  };
 });
 
-router.delete('/:id', (req, res) => {
-  // delete on tag by its `id` value
+router.delete('/:id', async (req, res) => {
+  // delete on tag by its `id` value - Complete
+  try {
+    const tagData = await Tag.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+    if (!tagData) {
+      res.status(404).json({ message: 'No match with this ID. Please try again.' });
+      return;
+    };
+    res.status(200).json(tagData);
+  } catch (err) {
+    res.status(400).json(err);
+  };
 });
 
 module.exports = router;
